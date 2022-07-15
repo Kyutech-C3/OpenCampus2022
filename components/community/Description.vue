f8e0803f6fcc5dcf01b6628ad369e2aaf114957a
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-07-15 21:55:10 +0900

    [change] コミュニティー説明文をhtml対応

--- components/community/Description.vue
+++ components/community/Description.vue
@@ -1,56 +1,56 @@
 <template>
-	<div class="allWrapper">
-		<p class="name bold">{{ info.name }}</p>
-		<p class="description">{{ info.description }}</p>
-	</div>
+  <div class="allWrapper">
+    <p class="name bold">{{ info.name }}</p>
+    <p class="description" v-html="info.description" />
+  </div>
 </template>
 
 <script>
 export default {
-props: {
-	info: {
-			type: Object,
-			required: true,
-			default: {
-				name: "default",
-				description: "default"
-			}
-		}
-	}
-}
+  props: {
+    info: {
+      type: Object,
+      required: true,
+      default: {
+        name: "default",
+        description: "default"
+      }
+    }
+  }
+};
 </script>
 
 <style lang="css" scoped>
-div{
-	--base-color: #495D7E;
-	--highlight-color: #FCFB6B;
+div {
+  --base-color: #495d7e;
+  --highlight-color: #fcfb6b;
 }
 .allWrapper {
-	margin: 0;
-	width: 100%;
+  margin: 0;
+  width: 100%;
 }
 .name {
-	color: var(--base-color);
-	background:linear-gradient(transparent 60%, var(--highlight-color) 60%);
-	font-size: calc(6.5vw + 4px);
-	display: inline-block;
-	white-space: nowrap;
-	padding-right: 10px;
-	margin-bottom: 3vw;
-	-webkit-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
-	user-select: none;
-	max-width: 100%;
-	resize: both;
+  color: var(--base-color);
+  background: linear-gradient(transparent 60%, var(--highlight-color) 60%);
+  font-size: calc(6.5vw + 4px);
+  display: inline-block;
+  white-space: nowrap;
+  padding-right: 10px;
+  margin-bottom: 3vw;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  max-width: 100%;
+  resize: both;
 }
 .description {
-	font-size: calc(0.5vw + 14px);
-	margin-left: 4%;
-	text-align: justify;
-	text-justify: auto;
+  font-size: calc(0.5vw + 14px);
+  margin-left: 4%;
+  text-align: justify;
+  text-justify: auto;
 }
-.description::selection{
-	background: #4565863d;
+.description::selection {
+  background: #4565863d;
 }
 </style>
